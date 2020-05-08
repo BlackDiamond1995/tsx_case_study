9abe6c0b991e406f6814c4eaa6b0281ab5b83445
BlackDiamond1995 <luyaosong19951022@163.com>
2020-05-09 01:45:57 +0800

    change targetfile size from 100M to 2G

--- generate_page_cache_file.sh
+++ generate_page_cache_file.sh
@@ -5,7 +5,7 @@ cd $pagecacheFilePath
 
 dd if=/dev/zero of=lockfile2G bs=1G count=2
 
-dd if=/dev/zero of=targetfile bs=1M count=100
+dd if=/dev/zero of=targetfile bs=1G count=2
 
 for i in 0 1 2 3 4 5 6 7
 do
